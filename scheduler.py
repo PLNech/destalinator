87d0b8696af9055c03fa703ea4df59c229d08d89
Paul-Louis NECH <pln@algolia.com>
2017-04-24 17:33:02 +0200

    scheduler: Disable warner and archiver

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import logging
-import warner
-import archiver
+# import warner
+# import archiver
 import announcer
 import flagger
 import os
@@ -9,21 +9,21 @@ import os
 logging.basicConfig()
 sched = BlockingScheduler()
 
-@sched.scheduled_job("cron", hour=4)
-#@sched.scheduled_job("cron", hour="*", minute="*/10") # for testing
+@sched.scheduled_job("cron", hour=24)
+# @sched.scheduled_job("cron", hour="*", minute="*/10") # for testing
 def destalinate_job():
     print("Destalinating")
     if "SB_TOKEN" not in os.environ or "API_TOKEN" not in os.environ:
         print("ERR: Missing at least one Slack environment variable.")
     else:
-        scheduled_warner = warner.Warner()
-        scheduled_archiver = archiver.Archiver()
+        # scheduled_warner = warner.Warner()
+        # scheduled_archiver = archiver.Archiver()
         scheduled_announcer = announcer.Announcer()
         scheduled_flagger = flagger.Flagger()
-        print("Warning")
-        scheduled_warner.warn()
-        print("Archiving")
-        scheduled_archiver.archive()
+        # print("Warning")
+        # scheduled_warner.warn()
+        # print("Archiving")
+        # scheduled_archiver.archive()
         print("Announcing")
         scheduled_announcer.announce()
         print("Flagging")
